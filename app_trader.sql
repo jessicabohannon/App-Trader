243bcbe5db22247620631ce7a3662b644a628331
jessicabohannon <jessicabohannon5@gmail.com>
2024-02-24 11:31:10 -0600

    Draft

--- app_trader.sql
+++ app_trader.sql
@@ -1,4 +1,4 @@
-WITH apps AS 
+WITH apps AS 	--unioning the 2 tables, filtering for games
 (
 	(SELECT name, rating, review_count::numeric, price
 	FROM app_store_apps
@@ -17,15 +17,15 @@ SELECT
 	MAX(price) AS price,
 	CASE WHEN MAX(price) <= 1 THEN 10000 
 		ELSE MAX(price)*10000 END 
-		AS purchase_price,
+		AS purchase_price,		--purchase price is app_price * 10000, with a min of 10000
 	(CASE WHEN name IN (SELECT name FROM app_store_apps) THEN 5000 ELSE 0 END +
 		CASE WHEN name IN (SELECT name FROM play_store_apps) THEN 5000 ELSE 0 END) * 12 
-		AS yearly_income, 
+		AS yearly_income, 	--income is 5000 per app store it is on; negated duplicates
 	CASE WHEN ROUND(AVG(rating)*2, 0) / 2 = 5.0 THEN 11 
 		WHEN ROUND(AVG(rating)*2, 0) / 2 = 4.5 THEN 10 
 		WHEN ROUND(AVG(rating)*2, 0) / 2 = 4.0 THEN 9 
 		END 
-		AS lifespan,
+		AS lifespan,		--lifespan dependent on rating
 	ROUND(
 		(((CASE WHEN name IN (SELECT name FROM app_store_apps) THEN 5000 ELSE 0 END +
 			CASE WHEN name IN (SELECT name FROM play_store_apps) THEN 5000 ELSE 0 END) * 12)
@@ -41,7 +41,7 @@ SELECT
 		- 
 		(CASE WHEN MAX(price) <= 1 THEN 10000 ELSE MAX(price)*10000 END)
 		, 0)
-		AS net_profit -- (yearly_income * lifespan) - (lifespan * 1000) - purchase price
+		AS net_profit --(yearly_income * lifespan) - (lifespan * marketing costs(1000) - purchase price
 FROM apps
 GROUP BY name
 HAVING ROUND(ROUND(AVG(rating)*2, 0) / 2, 1) >= 4.0
